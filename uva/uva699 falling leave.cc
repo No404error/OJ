24ee913ea1be845bc5730fcf24b5048c0710c0be
Zhang <3484868205@qq.com>
2021-10-20 22:00:36 +0800

    递归过程

--- uva/uva699 falling leave.cc
+++ uva/uva699 falling leave.cc
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int maxn=1000;
+const int maxn=1000; 
 int sumt[maxn];
-void build(int p){
+void build(int p){ //建树
     int t;cin>>t;
-    if(t==-1) return;
-    sumt[p]+=t;
-    build(p-1);build(p+1);
+    if(t==-1) return; //无树
+    sumt[p]+=t; //添加值
+    build(p-1);build(p+1); //建左子树和右子树
 }
-bool init(){
+bool init(){ 
     int t;cin>>t;
-    if(t==-1) return false;
-    memset(sumt,0,sizeof(sumt));
-    int pos=maxn/2;
-    sumt[pos]=t;
-    build(pos-1);build(pos+1);
+    if(t==-1) return false; //-1为结束条件
+    memset(sumt,0,sizeof(sumt)); //重置
+    int pos=maxn/2; //合理利用内存
+    sumt[pos]=t; //根
+    build(pos-1);build(pos+1); //左子树和右子树
     return true;
 }
 int main(){
