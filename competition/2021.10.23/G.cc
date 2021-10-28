1a3f65b779c7b9658017146c7cf00929f9c26461
Zhang <3484868205@qq.com>
2021-10-28 19:58:20 +0800

    格式题

--- competition/2021.10.23/G.cc
+++ competition/2021.10.23/G.cc
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 long long lis[]={127,32767,2147483647,9223372036854775807},test;
-string st[]={"byte","short","int","long","BigInteger"}; 
+string st[]={"byte","short","int","long","BigInteger","9223372036854775807"};
 int main(){
     string s,ans;
-    while(cin>>s){
-       if(s.size()>19) ans=st[4];
+    while(cin>>s){ 
+       if(s.size()>19) ans=st[4]; //大于longlong
        else{
-           stringstream p(s.substr(0,19));
-           p>>test;
-           if(s.size()==19) {
-                if(test<0) ans=st[4];
-                else ans=st[3];
+           if(s.size()==19) { 
+                if(s>st[5]) ans=st[4]; //19位且大于longlong
+                else ans=st[3]; //为int
            }
            else{
-               if(test<=lis[0]) ans=st[0];
+               stringstream p(s.substr(0,19)); //把字符串转为long long
+               p>>test;
+               if(test<=lis[0]) ans=st[0]; 
                else if(test<=lis[1]) ans=st[1];
                else if(test<=lis[2]) ans=st[2];
                else ans=st[3];
